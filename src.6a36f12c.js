018a6ba3b4a9d9193aa66eccfff8e36f6c65427d
Oliko136 <89538242+Oliko136@users.noreply.github.com>
2021-12-03 12:26:09 +0000

    Deploying to gh-pages from @ Oliko136/project-16-icecream@c0c49913aadf587b46489f08274739f55db40aaa 🚀

--- src.6a36f12c.js
+++ src.6a36f12c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-16-icecream/src.e6c16e35.js.map
+//# sourceMappingURL=/project-16-icecream/src.6a36f12c.js.map
